840564d7345a11344a02385158d4fcd80e7c57af
code_williams <williams.ca001@gmail.com>
2022-01-11 19:43:14 +0330

    Styles fixed | Countdown timer converted to Persian

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,7 +80,7 @@
             $('.time-countdown').each(function() {
             var $this = $(this), finalDate = $(this).data('countdown');
             $this.countdown(finalDate, function(event) {
-                var $this = $(this).html(event.strftime('' + '<div class="counter-column"><div class="inner"><span class="count">%D</span>Days</div></div> ' + '<div class="counter-column"><div class="inner"><span class="count">%H</span>Hours</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%M</span>Mins</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%S</span>Secs</div></div>'));
+                var $this = $(this).html(event.strftime('' + '<div class="counter-column"><div class="inner"><span class="count">%D</span>روز</div></div> ' + '<div class="counter-column"><div class="inner"><span class="count">%H</span>ساعت</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%M</span>دقیقه</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%S</span>ثانیه</div></div>'));
             });
          });
         }
